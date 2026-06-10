2c3327035523affeaa2cd572675950db35ec3d9a
Atyam Jayita <jayita.atyam1808@gmail.com>
2026-06-10 23:47:47 +0530

    Time: 26 ms (6.6%), Space: 76.6 MB (95.01%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,13 +1,24 @@
-class Solution
-{
-    public boolean containsDuplicate(int[] nums)
-    {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
+/*class Solution{
+    public boolean containsDuplicate(int[] nums){
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] == nums[j]){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}*/
+import java.util.*;
+
+class Solution{
+    public boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++){
+            if(nums[i] == nums[i + 1]){
                 return true;
             }
-            map.put(nums[i],1);
         }
         return false;
     }
